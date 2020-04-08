fd242a55072e9ee29da84c026705c8711bc6711c
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2020-04-08 21:02:51 +0200

    :tada: Initial commit

---
+++ src/main/groovy/be/ducobu/inTime/InTimeApplication.groovy
@@ -0,0 +1,13 @@
+package be.ducobu.inTime
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class InTimeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(InTimeApplication, args)
+	}
+
+}
---
+++ src/test/groovy/be/ducobu/inTime/InTimeApplicationTests.groovy
@@ -0,0 +1,13 @@
+package be.ducobu.inTime
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

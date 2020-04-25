5b20765a4ff5cbec9ceb13b22c4af64bb9513f60
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2020-04-25 15:03:46 +0200

    :fire: Remove unneeded id in DTO

--- src/main/groovy/be/ducobu/inTime/dto/workspace/WorkspaceCreateDto.groovy
+++ src/main/groovy/be/ducobu/inTime/dto/workspace/WorkspaceCreateDto.groovy
@@ -2,16 +2,7 @@ package be.ducobu.inTime.dto.workspace
 
 class WorkspaceCreateDto {
 
-    private Long id
     private String name
-
-    Long getId() {
-        return id
-    }
-
-    void setId(Long id) {
-        this.id = id
-    }
 
     String getName() {
         return name

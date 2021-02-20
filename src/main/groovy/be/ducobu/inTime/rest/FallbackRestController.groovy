9a2e549d9741a1d48fa6b93616f7974082786c73
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2021-02-20 22:22:10 +0100

    Add Swagger UI (for tests purpose)

--- src/main/groovy/be/ducobu/inTime/rest/FallbackRestController.groovy
+++ src/main/groovy/be/ducobu/inTime/rest/FallbackRestController.groovy
@@ -1,16 +1,14 @@
 package be.ducobu.inTime.rest
 
-import be.ducobu.inTime.exception.WrongRestCallException
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Validated
 class FallbackRestController {
 
-    @GetMapping("/**")
-    void allFallback() {
-        throw new WrongRestCallException()
-    }
+//    @GetMapping("/**")
+//    void allFallback() {
+//        throw new WrongRestCallException()
+//    }
 }

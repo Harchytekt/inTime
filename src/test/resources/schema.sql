db7fa45bf81b4114eff085a530a00eba4af1370b
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2021-12-22 15:26:31 +0100

    [FIX] Fix tabs (replaced with spaces)

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -4,46 +4,46 @@
 -- CREATE DATABASE IF NOT EXISTS in_time;
 
 CREATE TABLE IF NOT EXISTS workspaces (
-	id INT NOT NULL AUTO_INCREMENT,
-	toggl_id INT,
-	name VARCHAR(50) NOT NULL,
-	PRIMARY KEY (id),
-	UNIQUE (name, toggl_id)
+    id INT NOT NULL AUTO_INCREMENT,
+    toggl_id INT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (name, toggl_id)
 );
 
 CREATE TABLE IF NOT EXISTS clients (
-	id INT NOT NULL AUTO_INCREMENT,
-	toggl_id INT,
-	name VARCHAR(50) NOT NULL,
-	fk_workspace INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (fk_workspace) REFERENCES workspaces(id),
-	UNIQUE (name, toggl_id)
+    id INT NOT NULL AUTO_INCREMENT,
+    toggl_id INT,
+    name VARCHAR(50) NOT NULL,
+    fk_workspace INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_workspace) REFERENCES workspaces(id),
+    UNIQUE (name, toggl_id)
 );
 
 CREATE TABLE IF NOT EXISTS projects (
-	id INT NOT NULL AUTO_INCREMENT,
-	toggl_id INT,
-	name VARCHAR(50) NOT NULL,
-	billable BOOLEAN NOT NULL DEFAULT FALSE,
-	fk_client INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (fk_client) REFERENCES clients(id),
-	UNIQUE (name, toggl_id)
+    id INT NOT NULL AUTO_INCREMENT,
+    toggl_id INT,
+    name VARCHAR(50) NOT NULL,
+    billable BOOLEAN NOT NULL DEFAULT FALSE,
+    fk_client INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_client) REFERENCES clients(id),
+    UNIQUE (name, toggl_id)
 );
 
 CREATE TABLE IF NOT EXISTS time_entries (
-	id INT NOT NULL AUTO_INCREMENT,
-	toggl_id INT,
-	start_date TIMESTAMP NOT NULL DEFAULT NOW(),
-	end_date TIMESTAMP,
-	duration INT,
-	description VARCHAR(256),
-	running BOOLEAN NOT NULL DEFAULT TRUE,
-	fk_project INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (fk_project) REFERENCES projects(id),
-	UNIQUE (toggl_id)
+    id INT NOT NULL AUTO_INCREMENT,
+    toggl_id INT,
+    start_date TIMESTAMP NOT NULL DEFAULT NOW(),
+    end_date TIMESTAMP,
+    duration INT,
+    description VARCHAR(256),
+    running BOOLEAN NOT NULL DEFAULT TRUE,
+    fk_project INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_project) REFERENCES projects(id),
+    UNIQUE (toggl_id)
 );
 
 -- Data Insertion

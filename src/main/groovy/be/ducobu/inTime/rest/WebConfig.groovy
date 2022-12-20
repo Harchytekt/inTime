0052fa9a811f73ea33d2153641ca5f519c3a178c
Alexandre Ducobu <alexandre.ducobu@pm.me>
2022-12-20 11:40:14 +0100

    [FEAT] Add CORS support to use Fetch API (JS)

---
+++ src/main/groovy/be/ducobu/inTime/rest/WebConfig.groovy
@@ -0,0 +1,17 @@
+package be.ducobu.inTime.rest
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+    // CORS enabled: https://www.baeldung.com/spring-cors
+}
